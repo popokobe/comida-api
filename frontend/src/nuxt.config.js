8cc05c990a6fc4ffc7ded31bfca71550b569c6ab
popokobe <yuki.n3kobe@gmail.com>
2020-04-27 23:44:51 +0900

    Configured basic settings for nuxt.

--- frontend/src/nuxt.config.js
+++ frontend/src/nuxt.config.js
@@ -1,3 +1,5 @@
+import colors from "vuetify/es5/util/colors";
+
 module.exports = {
   /*
    ** Headers of the page
@@ -15,6 +17,50 @@ module.exports = {
    ** Customize the progress bar color
    */
   loading: { color: "#3B8070" },
+  modules: ["@nuxtjs/vuetify", "@nuxtjs/axios", "@nuxtjs/auth"],
+  axios: {
+    baseURL: "http://127.0.0.1:8000"
+  },
+  // auth: {
+  //   redirect: {
+  //     login: "/login/", // 未ログイン時に認証ルートへアクセスした際のリダイレクトURL
+  //     logout: "/login/", // ログアウト時のリダイレクトURL
+  //     home: "/" // ログイン後のリダイレクトURL
+  //   },
+  //   strategies: {
+  //     local: {
+  //       endpoints: {
+  //         login: { url: "/auth/login/", method: "post", propertyName: "token" },
+  //         user: { url: "/auth/myinfo/", method: "get", propertyName: false }
+  //       },
+  //       tokenType: "jwt"
+  //     }
+  //   }
+  // },
+  // router: {
+  //   middleware: ["auth"]
+  // },
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
   /*
    ** Build configuration
    */
